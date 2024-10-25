71c361f4013480e5a0f008bc6fd58969a62ad99c
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2024-10-25 14:27:32 -0300

    fix: consertado estilos login

--- node/src/pages/Login.tsx
+++ node/src/pages/Login.tsx
@@ -1,12 +1,13 @@
-import { Box, Paper, TextField, Typography } from "@mui/material";
+import { Box, Button, Link, Paper, TextField, Typography } from "@mui/material";
 import logo from "../assets/logo.svg"
 
 import { styled } from '@mui/material/styles';
+import { Link as RouterLink } from "react-router-dom";
 
 const StyledBox = styled(Box)({
     display: "flex",
     gap: "1.875rem",
-    "@media (max-width: 600px)" : {
+    "@media (max-width: 600px)": {
         flexDirection: "column"
     }
 });
@@ -19,6 +20,18 @@ const StyledPaper = styled(Paper)({
     justifyContent: "center",
     flexDirection: "column",
     textAlign: "center",
+    paddingLeft: "0.5rem",
+    paddingRight: "0.5rem",
+    paddingTop: "4rem",
+    paddingBottom: "2rem",
+    width: "50%",
+    "@media (max-width: 600px)": {
+        minHeight: "35rem",
+        gap: "1.875rem",
+        width: "90%",
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+    }
 });
 
 const Title = styled(Typography)({
@@ -33,7 +46,6 @@ const Text = styled(Typography)({
     marginTop: "2.5rem",
     fontWeight: "bold",
 });
-
 
 
 export function Login() {
@@ -52,8 +64,72 @@ export function Login() {
                 <Title variant="h4">
                     LOGIN DO ASSOCIADO
                 </Title>
-                <TextField size="small" placeholder="CPF"/>
-                <TextField size="small" placeholder="Senha"/>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        minWidth: "80%",
+                        justifyContent: "space-between",
+                        gap: "1rem",
+                        marginTop: "2rem",
+                        marginBottom: "2rem",
+                        flexGrow: 1,
+
+                    }}
+                >
+
+                    <TextField size="small" placeholder="CPF" />
+                    <TextField size="small" placeholder="Senha" />
+
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Button variant="outlined" color="error">
+                            Entrar
+                        </Button>
+                        <Link
+                            sx={{
+                                fontWeight: "medium",
+                                color: "#BF1515",
+                                textDecoration: "none",
+                                "&:hover": {
+                                    textDecoration: "underline"
+                                }
+                            }}
+                            component={RouterLink}
+                            to={"/recuperar-senha"}
+                        >
+                            Esqueci a senha
+                        </Link>
+                    </Box>
+                    <Box
+                        sx={{
+                            marginTop: "auto",
+                            fontWeight: "medium",
+                            fontSize: "large"
+                        }}
+                    
+                    >
+                        Não é associado?
+                        <Link
+                            sx={{
+                                color: "#BF1515",
+                                textDecoration: "none",
+                                "&:hover": {
+                                    textDecoration: "underline"
+                                }
+                            }}
+                            component={RouterLink}
+                            to={"/cadastro"}
+                        >
+                            {" Clique aqui"}
+                        </Link>
+                    </Box>
+                </Box>
             </StyledPaper>
         </StyledBox>
     );
