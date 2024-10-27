bb0d06e4db2e45eba9ef3195bc330490ad03728e
Erik Gabriel <126505086+erikgabriel07@users.noreply.github.com>
2024-10-27 20:39:01 -0300

    Add files via upload

--- src/routes/login.py
+++ src/routes/login.py
@@ -1,10 +1,12 @@
 from flask import request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_refresh_token
 from werkzeug.security import check_password_hash
 
 from database.sessao import db
 from model.login import Login
 from settings.jwt_manager import JWTManager
 from settings.limiter import RateLimiter
+from datetime import timedelta
 
 
 def register_routes_login(app, jwt_manager, rate_limiter):
@@ -24,7 +26,16 @@ def register_routes_login(app, jwt_manager, rate_limiter):
         if not user or not check_password_hash(user.senha, data['senha']):
             return jsonify({'message': 'Credenciais inválidas!'}), 401
 
-        #Gera o token JWT
+        # Gera o token JWT
         token = JWTManager(app.config['SECRET_KEY']).generate_jwt(user.id)
 
         return jsonify({'token': token}), 200
+
+    @app.route('/token/refresh', methods=['POST'])
+    @jwt_required(refresh=True)
+    def refresh_token():
+        """Rota para a criação de um token de acesso"""
+        identity = get_jwt_identity()
+        access_token = create_refresh_token(identity=identity, expires_delta=timedelta(days=30))
+
+        return jsonify({'refresh': access_token}), 200
