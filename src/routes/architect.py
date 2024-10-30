1dd1316d57a215f1923c8f9d0da6c74a4a1f06c1
Arthur Fernando <arthurfernando18@gmail.com>
2024-10-30 19:06:03 -0300

    feat: #6 adiciona crud de arquitetos

--- src/routes/architect.py
+++ src/routes/architect.py
@@ -12,9 +12,9 @@ from model.endereco import Endereco
 def register_routes_architect(app, token_authenticator):
     """Architect routes register"""
 
-    @app.route('/architect', methods=['POST'])
+    @app.route('/arquiteto/cadastrar', methods=['POST'])
     @token_authenticator.token_required
-    def create_architect():
+    def cadastrar_arquiteto():
         """Create a new architect."""
         data = request.get_json()
         endereco_domain = EnderecoDomain(data.get('endereco'))
@@ -22,13 +22,9 @@ def register_routes_architect(app, token_authenticator):
         novo_arquiteto = Arquiteto(
             nome=data['nome'],
             cpf=data['cpf'],
-            uf_documento=data['uf_documento'],
             matricula=data['matricula'],
             celular=data['celular'],
             fixo=data['fixo'],
-            data_filiacao=datetime.strptime(data['data_filiacao'], '%Y-%m-%d'),
-            data_fim_filiacao=datetime.strptime(data['data_fim_filiacao'], '%Y-%m-%d') if data.get(
-                'data_fim_filiacao') else None,
             endereco_id=endereco.id,
             email=data['email'],
             hash=data['hash'],
@@ -36,5 +32,97 @@ def register_routes_architect(app, token_authenticator):
         )
         db.session.add(novo_arquiteto)
         db.session.commit()
-        return jsonify({"message": "Arquiteto criado com sucesso!"}), 201
+        return jsonify({'message': 'Arquiteto criado com sucesso!'}), 201
 
+    @app.route('/arquiteto/buscar', methods=['GET'])
+    @token_authenticator.token_required
+    def listar_arquitetos(user_id=None):
+        nome = request.args.get('nome')
+        cpf = request.args.get('cpf')
+        matricula = request.args.get('matricula')
+        email = request.args.get('email')
+
+        query = Arquiteto.query.filter_by(fl_ativo=1)
+
+        if nome:
+            query = query.filter(Arquiteto.nome.ilike(f'%{nome}%'))
+        if cpf:
+            query = query.filter_by(cpf=cpf)
+        if matricula:
+            query = query.filter_by(matricula=matricula)
+        if email:
+            query = query.filter(Estudante.email.ilike(f'%{email}%'))
+
+        arquitetos = query.all()
+        resultados = []
+        for arquiteto in arquitetos:
+            endereco = arquiteto.endereco
+            result = {
+                'id': arquiteto.id,
+                'nome': arquiteto.nome,
+                'cpf': arquiteto.cpf,
+                'matricula': arquiteto.matricula,
+                'celular': arquiteto.celular,
+                'fixo': arquiteto.fixo,
+                'data_filiacao': arquiteto.data_filiacao,
+                'data_fim_filiacao': arquiteto.data_fim_filiacao,
+                'endereco': {
+                    'cep': endereco.cep,
+                    'logradouro': endereco.logradouro,
+                    'complemento': endereco.complemento,
+                    'numero': endereco.numero,
+                    'bairro': endereco.bairro,
+                    'cidade': endereco.cidade,
+                    'estado': endereco.estado
+                },
+                'email': arquiteto.email,
+                'hash': arquiteto.hash,
+                'foto': arquiteto.foto,
+            }
+            resultados.append(result)
+        return jsonify(resultados), 200
+
+    @app.route('/arquiteto/atualizar/<int:id>', methods=['PUT'])
+    @token_authenticator.token_required
+    def atualizar_arquiteto(id, user_id=None):
+
+        data = request.get_json()
+
+        arquiteto = Arquiteto.query.get(id)
+
+        if not arquiteto:
+            return jsonify({'message': 'Arquiteto não encontrado.'}), 404
+
+        arquiteto.nome = data.get('nome', arquiteto.nome)
+        arquiteto.matricula = data.get('matricula', arquiteto.matricula)
+        arquiteto.celular = data.get('celular', arquiteto.celular)
+        arquiteto.fixo = data.get('fixo', arquiteto.fixo)
+        arquiteto.email = data.get('email', arquiteto.email)
+        arquiteto.foto = data.get('foto', arquiteto.foto)
+
+        if arquiteto.endereco:
+            endereco = arquiteto.endereco
+            endereco.cep = data.get('endereco', {}).get('cep', endereco.cep)
+            endereco.logradouro = data.get('endereco', {}).get('logradouro', endereco.logradouro)
+            endereco.complemento = data.get('endereco', {}).get('complemento', endereco.complemento)
+            endereco.numero = data.get('endereco', {}).get('numero', endereco.numero)
+            endereco.bairro = data.get('endereco', {}).get('bairro', endereco.bairro)
+            endereco.cidade = data.get('endereco', {}).get('cidade', endereco.cidade)
+            endereco.estado = data.get('endereco', {}).get('estado', endereco.estado)
+
+        db.session.commit()
+
+        return jsonify({'message': 'Arquiteto atualizado com sucesso!'}), 200
+
+    @app.route('/arquiteto/excluir/<int:id>', methods=['PUT'])
+    @token_authenticator.token_required
+    def deletar_arquiteto(id, user_id=None):
+        arquiteto = Arquiteto.query.get(id)
+
+        if not arquiteto:
+            return jsonify({'message': 'Arquiteto não encontrado'}), 404
+
+        arquiteto.fl_ativo = 0
+        db.session.commit()
+
+        return jsonify({'message': 'Arquiteto inativado com sucesso!'})
