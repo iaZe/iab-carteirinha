356a932b898d4590ef3bfafe40a007c44bca98c0
Daniel Tavares <nieltvrs@gmail.com>
2024-10-31 13:13:45 -0300

    edit redis

--- src/settings/config.py
+++ src/settings/config.py
@@ -20,5 +20,6 @@ class Config:
         host=os.getenv("REDIS_HOST"),
         port=int(os.getenv("REDIS_PORT")),
         password=os.getenv("REDIS_PASSWORD"),
+        db = 0,
         decode_responses=True
     )
