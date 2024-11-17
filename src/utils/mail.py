d1bba41d445b80251a91383919b8935970b57746
Arthur Fernando <afssantos@sefaz.se.gov.br>
2024-11-17 16:57:19 -0300

    feat: #10 adiciona funcionalidade de envio de e-mail (WIP)

---
+++ src/utils/mail.py
@@ -0,0 +1,31 @@
+from flask_mail import Mail, Message
+from utils.token import gerar_token_confirmacao
+from flask import url_for
+
+import os
+
+mail = Mail()
+
+
+def enviar_email_confirmacao(email, nome):
+ try:
+     token = gerar_token_confirmacao(email)
+     if os.getenv('FLASK_ENV') == 'production':
+         link_confirmacao = f"https://iabapptest1-87j8tiit.b4a.run/arquiteto/confirmar/{token}"
+     else:
+         link_confirmacao = f"http://127.0.0.1:5000/arquiteto/confirmar/{token}"
+
+     msg = Message(
+         subject="Confirmação de Cadastro",
+         recipients=[email],
+         body=f"""Olá {nome},
+
+Por favor, clique no link abaixo para confirmar seu cadastro:
+{link_confirmacao}
+
+Obrigado por se juntar a nós!
+"""
+     )
+     mail.send(msg)
+ except Exception as e:
+     print(f"Erro ao enviar email: {e}")
