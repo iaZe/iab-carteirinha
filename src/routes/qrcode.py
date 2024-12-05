5abc5c06bb6ad053226a852fc48c0fb5b0c941f6
Erik Gabriel <126505086+erikgabriel07@users.noreply.github.com>
2024-12-05 12:39:28 -0300

    Update qrcode.py

--- src/routes/qrcode.py
+++ src/routes/qrcode.py
@@ -21,6 +21,6 @@ def registro_rota_qrcode(app, rate_limiter):
 
         qr.save(buffer, kind='png', scale=5, border=1)
 
-        buffer.seek(0) # Movendo o indicador do buffer para ler os dados do início
+        buffer.seek(0) # Movendo o indicador do buffer para ler os dados a partir do início
 
         return send_file(buffer, mimetype='image/png', as_attachment=False), 200
