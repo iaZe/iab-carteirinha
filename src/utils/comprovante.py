13b1e9c4cd2607262ca6ef6aa84871e0bb351fc9
Givaldo Andrade <contato.givaldoandrade@gmail.com>
2024-12-11 13:04:58 -0300

    Remover a pasta iab-carteirinha.git após reescrever o histórico

--- src/utils/comprovante.py
+++ src/utils/comprovante.py
@@ -1,8 +1,8 @@
 def gerar_comprovante(arquiteto):
     comprovante_path = f"comprovante_{arquiteto.id}.pdf"
   
-    with open(comprovante_path, 'w') as f: # Para fins de teste de conexão com Bucket "Givaldo"
-        f.write('Comprovante de renovação de carteirinha')
+    # with open(comprovante_path, 'w') as f: # Para fins de teste de conexão com Bucket "Givaldo"
+    #     f.write('Comprovante de renovação de carteirinha')
 
     print(f'Comprovante gerado: {comprovante_path}')
     return comprovante_path
